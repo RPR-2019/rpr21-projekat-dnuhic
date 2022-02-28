4098c3640277574af773aa6ff568d345790fc348
Dzenan Nuhic <dnuhic1@etf.unsa.ba>
2022-02-28 14:37:22 +0100

    Podesen projekat

---
+++ src/ba/unsa/etf/rpr/LogInController.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LogInController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
