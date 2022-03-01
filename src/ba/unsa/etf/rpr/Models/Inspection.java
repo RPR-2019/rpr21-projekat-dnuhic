fac2346213e039ff8a67c7f0835f75c3bd4451ec
Dzenan Nuhic <dnuhic1@etf.unsa.ba>
2022-03-01 21:03:01 +0100

    Dodani neki prevodi

--- src/ba/unsa/etf/rpr/Models/Inspection.java
+++ src/ba/unsa/etf/rpr/Models/Inspection.java
@@ -1,7 +1,10 @@
 package ba.unsa.etf.rpr.Models;
 
+import java.time.LocalDateTime;
+
 public class Inspection {
     private Person inspector;
     private CommercialBuilding building;
-
+    private InspectionResult inspectionResult;
+    private LocalDateTime inspectionDate;
 }
